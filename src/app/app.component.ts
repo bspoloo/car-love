135204d39a90e3650fca2201afd92284701a28d5
bspoloo <animetx098@gmail.com>
2025-03-01 00:09:17 -0400

    ready to deploy 2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,25 +1,33 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { LetterComponent } from "./components/letter/letter.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, CommonModule],
+  imports: [RouterOutlet, CommonModule, LetterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   public title = 'card-love';
   public isOpen : boolean;
+  public isChanged : boolean;
   constructor(){
     this.isOpen = false;
+    this.isChanged = false;
   }
   public open() : void{
-    console.log('is open');
+    this.changeState();
     this.isOpen = true;
   }
   public close() : void{
     console.log('is close');
     this.isOpen = false;
+  }
+  public changeState() : void{
+    setInterval(() => {
+      this.isChanged = true;
+    }, 7000);
   }
 }
